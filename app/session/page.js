7e1a377352fef5748dd94c7f1c15c3e86c173932
fuglsetm <fuglsetm@gmail.com>
2024-10-02 12:57:58 +0000

    add alarm siren

--- app/session/page.js
+++ app/session/page.js
@@ -1,7 +1,7 @@
 export default function Session() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-800 text-white">
-      <h1 className="text-3xl font-bold">Session Started!</h1>
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <img src="/Opening_movie.png" alt="Opening Movie" />
     </div>
   );
 }
